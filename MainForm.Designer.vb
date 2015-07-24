7cfbdaecbba24f8d883ec32c81ee7379a586b77b
Shane <shane8641@live.com>
2015-07-24 17:22:34 -0400

    First commit

---
+++ MainForm.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGuessNum = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnGuessNum
+        '
+        Me.btnGuessNum.Location = New System.Drawing.Point(60, 102)
+        Me.btnGuessNum.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.btnGuessNum.Name = "btnGuessNum"
+        Me.btnGuessNum.Size = New System.Drawing.Size(138, 35)
+        Me.btnGuessNum.TabIndex = 1
+        Me.btnGuessNum.Text = "&Guess"
+        Me.btnGuessNum.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(278, 102)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(138, 35)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblMessage
+        '
+        Me.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblMessage.Font = New System.Drawing.Font("Showcard Gothic", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMessage.Location = New System.Drawing.Point(127, 24)
+        Me.lblMessage.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(229, 42)
+        Me.lblMessage.TabIndex = 3
+        Me.lblMessage.Text = "Guess My Number!!"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(506, 191)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnGuessNum)
+        Me.Font = New System.Drawing.Font("Showcard Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.Name = "MainForm"
+        Me.Text = "Guess Random Number App"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnGuessNum As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblMessage As System.Windows.Forms.Label
+
+End Class
---
+++ MainForm.vb
@@ -0,0 +1,56 @@
+﻿
+Option Strict On
+
+Public Class MainForm
+
+    Private intNumGuess As Integer
+    Private intCount As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Close the application
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnGuessNum_Click(sender As Object, e As EventArgs) Handles btnGuessNum.Click
+        Dim randGen As New Random
+        Dim intNum As Integer
+        Dim intNumGuess As Integer
+        Dim strPrompt As String = "Guess A Number between 1-50:"
+        Dim strTitle As String = "Guess Number"
+        Dim strPromptWin As String = "You guessed right!"
+        Dim strPromptLow As String = "Number is Greater!"
+        Dim strPromptHigh As String = "Number is Lower!"
+
+
+        ' assign random number from 1-50
+        intNum = randGen.Next(1, 51)
+        intNumGuess = CInt(InputBox(strPrompt, strTitle))
+        ' Loop through the game 10 times
+        For intCount As Integer = 0 To 9 Step 1
+            intNumGuess = CInt(InputBox(strPrompt, strTitle))
+            If intNumGuess = intNum Then
+                InputBox(strPromptWin, strTitle)
+                Exit Sub
+            ElseIf intNumGuess > intNum Then
+                InputBox(strPromptHigh, CStr(intCount))
+
+            ElseIf intNumGuess < intNum Then
+                InputBox(strPromptLow, CStr(intCount))
+
+            End If
+
+
+        Next intCount
+        ' Show user the correct number after failed attempts
+        MessageBox.Show("The correct number was " & intNum)
+
+
+
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        MessageBox.Show("Press Guess if you'd like to play a game...")
+    End Sub
+
+End Class
